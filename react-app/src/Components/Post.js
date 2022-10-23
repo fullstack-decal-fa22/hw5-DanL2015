474a196b740f5d0fe54006da8f4fb519dbd184e9
Daniel Liu <daniel17liu@gmail.com>
2022-10-23 09:55:59 -0700

    initial commit

--- react-app/src/Components/Post.js
+++ react-app/src/Components/Post.js
@@ -1,3 +1,4 @@
+import Comments from './Comments'
 // ___________________ import statement
 
 const Post = ({ title, body, comments, id }) => {
@@ -7,7 +8,7 @@ const Post = ({ title, body, comments, id }) => {
       {title}
     </h3>
     <p>{body}</p>
-    <Comments _________ _________ />
+    <Comments postId={id} comments={comments} />
   </div>
 }
 
